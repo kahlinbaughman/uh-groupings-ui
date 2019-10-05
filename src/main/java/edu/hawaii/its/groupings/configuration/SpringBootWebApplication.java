b5cfec97ac5bb8a78b1b53770af76fe97e8637e5
Kahlin Baughman <kahlin@hawaii.edu>
2019-10-04 16:35:49 -1000

    Update comments

--- src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
+++ src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
@@ -26,32 +26,34 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+
     /*
     * Post Construct runs the method only once after bean initialization, these will run if there
     * are no beans to initialize. This should run before the actual application runs.
      */
     @PostConstruct
-    private void checkForPw() throws IOException {
-        // Access the file regardless of the system it is on using Spring ClassPathResource
+    private void checkForPwd() throws IOException {
+        // Access the file regardless of the environment using Spring ClassPathResource.
+        // The file name should be unique to the project.
         File resource = new ClassPathResource("custom.properties").getFile();
         Scanner fileScanner = new Scanner(resource);
-        // Line the pattern is found on
+        // Tracker for the line the pattern is found on.
         int lineID = 0;
         ArrayList<Integer> lineNumbers = new ArrayList<>();
-        // Uses string to make a pattern to compare to
+        // Uses a string to make a pattern to compare to.
         Pattern pattern = Pattern.compile("grouperClient.webService.password");
         Matcher matcher = null;
         while(fileScanner.hasNextLine()) {
             String line = fileScanner.nextLine();
             lineID++;
-            // Creates a matcher object to look through the line given
+            // Creates a matcher object to look through the line given.
             matcher = pattern.matcher(line);
             if(matcher.find()) {
                 lineNumbers.add(lineID);
             }
         }
 
-        // If lineNumbers is not empty, spits out the error message to the terminal
+        // If lineNumbers is not empty, spits out the error message to the terminal.
         Assert.isTrue(lineNumbers.isEmpty(), "Please remove the password from the custom.properties file.");
     }
 
